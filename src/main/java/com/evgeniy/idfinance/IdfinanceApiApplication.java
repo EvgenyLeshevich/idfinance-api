fe20714e15760e0989630e3784acc020cfd953d3
Evgeniy Leshevich <evgeniy.le99@gmail.com>
2022-07-15 08:51:48 +0300

    Initial commit

---
+++ src/main/java/com/evgeniy/idfinance/IdfinanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.evgeniy.idfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdfinanceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdfinanceApiApplication.class, args);
+    }
+
+}
